c377c2acccd4defb61c1b73c62a82d8b31d82461
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2017-05-24 13:31:39 +0200

    Improve examples. Convert to unix files.

--- applications/geoTrellis/src/main/scala/read_hdfs.scala
+++ applications/geoTrellis/src/main/scala/read_hdfs.scala
@@ -1,9 +1,11 @@
-import geotrellis.raster.Tile
+import geotrellis.raster.{MultibandTile, Tile}
 import geotrellis.spark.io.hadoop._
 import geotrellis.vector.ProjectedExtent
 import org.apache.hadoop.fs.Path
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
-
+import org.apache.spark.sql.SparkSession
 
 /**
   * Created by Romulo on 5/18/2017.
@@ -14,15 +16,32 @@ object read_hdfs extends App {
     val masterURL="spark://emma0.emma.nlesc.nl:7077"
 
     val sc = new SparkContext(new SparkConf().setAppName(appName).setMaster(masterURL))
-    val filepath = "hdfs:///user/ubuntu/files"
-    val tilesDir = new Path(filepath)
-    //We are reading a Multi-band
-    val multiBandRDD = sc.hadoopGeoTiffRDD(tilesDir, ".tif")
 
-    val indexKey = multiBandRDD.map{case (k,v) => (v,k)}
-    val tile1 :Array[(Tile, ProjectedExtent)] = indexKey.take(1)
-    val res = tile1.map{case (t, p) => t}
-    val kmeansInput = res.toVector
+    //In Spark 2.1.1 we need a Session
+    val ss = SparkSession.builder().getOrCreate()
+    val filepath = "hdfs:///user/Romulo/spring_index/LastFreeze/1980.tif"
+    val tilesDir = new Path(filepath)
+
+    //We are reading a Single-band
+    val multiBandRDD : RDD[(ProjectedExtent, MultibandTile)] = sc.hadoopMultibandGeoTiffRDD(filepath)
+    val multiBandTiles = multiBandRDD.values
+    val multiBandTileItera = multiBandTiles.toLocalIterator
+    val singleBand_1 = multiBandTileItera.next().band(0)
+    val singleBand_2 = multiBandTileItera..next().band(0)
+    val singleBand_3 = multiBandTileItera.next().band(0)
+
+    val singleBand_1_RDD = sc.parallelize(singleBand_1.toArray())
+
+    val kmeans = new KMeans().setK(2).setSeed(1L)
+    val model = kmeans.fit(ss.createDataset(singleBand_1_RDD))
+
+    // Evaluate clustering by computing Within Set Sum of Squared Errors.
+    val WSSSE = model.computeCost(singInput)
+    println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+    // Shows the result.
+    println("Cluster Centers: ")
+    model.clusterCenters.foreach(println)
 
   }
 
