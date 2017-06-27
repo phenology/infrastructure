58d257dd9d779fb7b2e40168665faa5a37271ba6
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2017-06-27 09:55:55 +0200

    Latest version

--- applications/geoTrellis/src/main/scala/geoTrellis_multiTiffs_save_GeoTiff.scala
+++ applications/geoTrellis/src/main/scala/geoTrellis_multiTiffs_save_GeoTiff.scala
@@ -1,14 +1,20 @@
 import geotrellis.proj4.CRS
-import geotrellis.raster.{DoubleArrayTile, Tile}
+import geotrellis.raster.{ArrayTile, CellType, DoubleArrayTile, Tile, UByteCellType}
 import geotrellis.raster.io.geotiff._
 import geotrellis.raster.io.geotiff.writer.GeoTiffWriter
 import geotrellis.raster.io.geotiff.{GeoTiff, SinglebandGeoTiff}
 import geotrellis.spark.io.hadoop._
 import geotrellis.vector.{Extent, ProjectedExtent}
+import org.apache.spark.api.java.JavaPairRDD
+import org.apache.spark.ml.clustering.KMeansModel
 import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry, RowMatrix}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+
+//Spire is a numeric library for Scala which is intended to be generic, fast, and precise.
+import spire.syntax.cfor._
 
 object geoTrellis_multiTiffs_save_GeoTiff extends App {
 
@@ -23,11 +29,13 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
     val sample = 1000;
     var projected_extent = new ProjectedExtent(new Extent(0,0,0,0), CRS.fromName("EPSG:3857"))
     var num_cols_rows :(Int, Int) = (0, 0)
+    var cellT :CellType = UByteCellType
+
     var band_RDD: RDD[Array[Double]] = sc.emptyRDD
     var band_vec: RDD[Vector] = sc.emptyRDD
     var band0: RDD[(Long, Double)] = sc.emptyRDD
-    var band0_index: Array[Int] = Array.emptyIntArray
-    val pattern: String = "tif"
+    var band0_index: RDD[Long] = sc.emptyRDD
+    val pattern: String = "2.tif"
     var filepath: String = ""
     if (band_count == 1) {
       //Single band GeoTiff
@@ -44,18 +52,24 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
       val bands_RDD = tiles_RDD.map(m => m.toArrayDouble())
 
       val extents_withIndex = sc.hadoopGeoTiffRDD(filepath, pattern).keys.zipWithIndex().map{case (e,v) => (v,e)}
-      projected_extent = extents_withIndex.lookup(0).apply(0)
+      projected_extent = (extents_withIndex.filter(m => m._1 == 0).values.collect())(0)
 
       val tiles_withIndex = tiles_RDD.zipWithIndex().map{case (e,v) => (v,e)}
-      num_cols_rows = (tiles_withIndex.lookup(0).apply(0).cols, tiles_withIndex.lookup(0).apply(0).rows)
+      //num_cols_rows = (tiles_withIndex.lookup(0).apply(0).cols, tiles_withIndex.lookup(0).apply(0).rows)
+      val tile0 = (tiles_withIndex.filter(m => m._1==0).values.collect())(0)
+      num_cols_rows = (tile0.cols,tile0.rows)
+      cellT = tile0.cellType
+
 
       //Get Index for Cells
       val bands_withIndex = bands_RDD.zipWithIndex().map { case (e, v) => (v, e) }
       //band0_index = bands_withIndex.lookup(0).apply(0).zipWithIndex.filter{ case (v, i) => !v.isNaN }.map { case (v, i) => (i) }
-      val band0_index = bands_withIndex.lookup(0).apply(0).zipWithIndex.filter(m => !m._1.isNaN).take(sample).map { case (v, i) => (i) }
+      //val band0_index = bands_withIndex.lookup(0).apply(0).zipWithIndex.filter(m => !m._1.isNaN).take(sample).map { case (v, i) => (i) }
+      band0_index = bands_withIndex.filter(m => m._1 == 0).values.flatMap(m => m).zipWithIndex.filter(m => !m._1.isNaN).map { case (v, i) => (i) }
 
       //Get Array[Double] of a Title to later store the cluster ids.
-      band0 = sc.parallelize(bands_withIndex.lookup(0).take(1)).flatMap( m => m).zipWithIndex.map{case (v,i) => (i,v)}
+      //band0 = sc.parallelize(bands_withIndex.lookup(0).take(1)).flatMap( m => m).zipWithIndex.map{case (v,i) => (i,v)}
+      band0 = bands_withIndex.filter(m => m._1 == 0).values.flatMap( m => m).zipWithIndex.map{case (v,i) => (i,v)}
 
       //Lets filter out NaN
       band_RDD = bands_RDD.map(m => m.filter(!_.isNaN).take(sample))
@@ -66,20 +80,24 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
       val bands_RDD = tiles_RDD.map(m => m.band(3).toArrayDouble())
 
       val extents_withIndex = sc.hadoopGeoTiffRDD(filepath, pattern).keys.zipWithIndex().map{case (e,v) => (v,e)}
-      projected_extent = extents_withIndex.lookup(0).apply(0)
+      projected_extent = (extents_withIndex.filter(m => m._1 == 0).values.collect())(0)
 
       val tiles_withIndex = tiles_RDD.zipWithIndex().map{case (e,v) => (v,e)}
-      num_cols_rows = (tiles_withIndex.lookup(0).apply(0).cols, tiles_withIndex.lookup(0).apply(0).rows)
+      val tile0 = (tiles_withIndex.filter(m => m._1==0).values.collect())(0)
+      num_cols_rows = (tile0.cols,tile0.rows)
+      cellT = tile0.cellType
+
 
       //Get Index for Cells
       val bands_withIndex = bands_RDD.zipWithIndex().map { case (e, v) => (v, e) }
-      band0_index = bands_withIndex.lookup(0).apply(0).zipWithIndex.filter { case (v, i) => !v.isNaN }.take(sample).map { case (v, i) => (i) }
+      //band0_index = bands_withIndex.lookup(0).apply(0).zipWithIndex.filter { case (v, i) => !v.isNaN }.take(sample).map { case (v, i) => (i) }
+      band0_index = bands_withIndex.filter(m => m._1 == 0).values.flatMap(m => m).zipWithIndex.filter(m => !m._1.isNaN).map { case (v, i) => (i) }
 
       //Get Array[Double] of a Title to later store the cluster ids.
-      band0 = sc.parallelize(bands_withIndex.lookup(0).take(1)).flatMap( m => m).zipWithIndex.map{case (v,i) => (i,v)}
+      band0 = bands_withIndex.filter(m => m._1 == 0).values.flatMap( m => m).zipWithIndex.map{case (v,i) => (i,v)}
 
       //Let's filter out NaN
-      band_RDD = bands_RDD.map(m => m.filter(v => !v.isNaN).take(sample))
+      band_RDD = bands_RDD.map(m => m.filter(v => !v.isNaN))
     }
 
     /*
@@ -108,30 +126,39 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
       band_vec = sc.parallelize(band_vec_T).map(m => Vectors.dense(m)).cache()
     } else {
       //B) For large memory footpring RDDs we need to run in distributed mode
+      val mat :RowMatrix = new RowMatrix(band_RDD.map(m => Vectors.dense(m)))
 
       // Split the matrix into one number per line.
-      val byColumnAndRow = band_RDD.zipWithIndex.flatMap {
-        case (row, rowIndex) => row.zipWithIndex.map {
-          case (number, columnIndex) => columnIndex -> (rowIndex, number)
+      val byColumnAndRow = mat.rows.zipWithIndex.map {
+        case (row, rowIndex) => row.toArray.zipWithIndex.map {
+          case (number, columnIndex) => new MatrixEntry(rowIndex, columnIndex, number)
         }
-      }
+      }.flatMap(x => x)
+
+      val matt: CoordinateMatrix = new CoordinateMatrix(byColumnAndRow)
+      val mat_T = matt.transpose()
+
+      band_vec = mat_T.toRowMatrix().rows
+
+      println(byColumnAndRow.keys.collect().size)
 
       // Build up the transposed matrix. Group and sort by column index first.
       val byColumn = byColumnAndRow.groupByKey.sortByKey().values
 
       // Then sort by row index.
       val transposed = byColumn.map {
-        indexedRow => indexedRow.toSeq.sortBy(_._1).map(_._2)
-      }
+        indexedRow => indexedRow.toArray.sortBy(_._1).map(_._2)
+      }.map(m => m.map{ case (i, v) => (v)})
 
-      band_vec = transposed.map(m => Vectors.dense(m.toArray)).cache()
+      band_vec = transposed.map(m => Vectors.dense(m)).cache()
     }
 
     /*
      Here we will collect some info to see if the transpose worked correctly
     */
 
-    val band_vec_col = band_vec.collect()
+    val band_vec_col = band_vec.saveAsObjectFile("")
+    band_vec = sc.objectFile("")
 
     //Number of Columns, i.e., years
     println(band_vec_col.size)
@@ -159,6 +186,11 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
 
     //Un-persist the model
     band_vec.unpersist()
+    val cc2 = Vector.empty
+    val dd:Array[Vector] = Array.empty
+    val ll = new KMeansModel(Array.empty :Array[Vector])
+    val cc1 = Vectors.dense(Array.emptyDoubleArray).to
+    var cc :KMeansModel = new KMeansModel(Vectors.dense(Array.emptyDoubleArray))
 
     /*
      Cluster model's result management
@@ -199,7 +231,7 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
 
     //res_out.foreach(println)
     println(res_out.size)
-    println(band0_index.size)
+    //println(band0_index.size)
 
     /*
      Save the result as GeoTiff. However, it is not straightforward.
@@ -209,35 +241,56 @@ object geoTrellis_multiTiffs_save_GeoTiff extends App {
     */
 
     //Merge two RDDs
-    val cluster_cell_pos = res.repartition(1).zip(sc.parallelize(band0_index, 1))
+    //val cluster_cell_pos = res.repartition(1).zip(band0_index)
     //val cluster_cell_pos = res.zip(sc.parallelize(band0_index))
-    cluster_cell_pos.collect()//.take(50)
+    //cluster_cell_pos.collect()//.take(50)
+    val cluster_cell_pos = ((res.zipWithIndex().map{ case (v,i) => (i,v)}).join(band0_index.zipWithIndex().map{ case (v,i) => (i,v)})).map{ case (k,(v,i)) => (v,i)}
+    cluster_cell_pos.count()
+    res.count()
 
     /*
      Join the RDD with clusters with the Grid of cells from GeoTiff.
      Inspired in:
       https://stackoverflow.com/questions/31257077/how-do-you-perform-basic-joins-of-two-rdd-tables-in-spark-using-python
     */
-    val band0_0_1010 = band0.filterByRange(0,1010)
-    val grid_clusters = band0_0_1010.repartition(100).leftOuterJoin(cluster_cell_pos.map{ case (c,i) => (i.toLong, c)})
+    val cluster_cell_pos_idx = cluster_cell_pos.map{ case (c,i) => (i.toLong, c)}
+    val grid_clusters = band0.leftOuterJoin(cluster_cell_pos_idx)
+
     //val grid_clusters_res = grid_clusters.sortByKey(true).map{case (k, (v, c)) => if (c == None) (k, Double.NaN) else (k, c.get)}//.take(50).foreach(println)
     val grid_clusters_res = grid_clusters.sortByKey(true).map{case (k, (v, c)) => if (c == None) (k, -1.0) else (k, c.get.toDouble)}//.take(50).foreach(println)
-
-
+    val res3 = grid_clusters_res.values.map(m => m.toDouble)
     /*
      Create a GeoTiff and save to HDFS.
     */
 
-    val cluster_cells :Array[Double] = grid_clusters_res.values.map(m => m.toDouble).collect()
-    val cluster_tile = DoubleArrayTile(cluster_cells, num_cols_rows._1, num_cols_rows._2)
+    val cluster_cells :Array[Double] = grid_clusters_res.values.collect()
+    //Define a Tile
+    val cluster_cellsD = DoubleArrayTile(cluster_cells, num_cols_rows._1, num_cols_rows._2)
+
+    val cluster_tile2 = geotrellis.raster.DoubleArrayTile.empty(num_cols_rows._1, num_cols_rows._2)
+
+    cfor(0)(_ < num_cols_rows._1, _ + 1) { col =>
+      cfor(0)(_ < num_cols_rows._2, _ + 1) { row =>
+        val v = cluster_cellsD.get(col, row)
+        cluster_tile2.setDouble(col, row, v)
+      }
+    }
+
+    cluster_tile2..get(1, 50)
+
+    //Create GeoTiff
+    val geoTif = new SinglebandGeoTiff(cluster_tile, projected_extent.extent, projected_extent.crs, Tags.empty, GeoTiffOptions.DEFAULT)
+
+
+    //val cluster_tile = ArrayTile(cluster_cells, num_cols_rows._1, num_cols_rows._2)
+    val cluster_tile = ArrayTile(band0.values.collect(), num_cols_rows._1, num_cols_rows._2)
+    cluster_cellsD.g.toArrayDouble().take(50)
+    cluster_tile.toArrayDouble().take(50)
+
+    //val cluster_tile = DoubleArrayTile(cluster_cells, num_cols_rows._1, num_cols_rows._2)
     //val cluster_tile = DoubleArrayTile(cluster_cells, 101, 10)
     val geoTiff = SinglebandGeoTiff(cluster_tile, projected_extent.extent, projected_extent.crs, Tags.empty, GeoTiffOptions.DEFAULT)
-
-    sc.parallelize(geoTiff.toByteArray).saveAsObjectFile("hdfs:///users/emma/spring-index/BloomFinal/cluster.tif")
-
-    //Write to the local file system
-    //val path = "~/clusters.tif"
-    //GeoTiffWriter.write(geoTiff, path)
-
+    geoTif.tile.toArrayDouble().take(50)
+    sc.parallelize(GeoTiffWriter.write(geoTiff)).saveAsObjectFile("hdfs:///users/emma/spring-index/BloomFinal/clusters_3.tif")
   }
 }
